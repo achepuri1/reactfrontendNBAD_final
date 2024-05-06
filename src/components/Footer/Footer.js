fc2517849cd5685a1ca1e3b6acd3946197a24129
Anjali <anjali@Anjalis-Air.lan>
2024-05-05 23:40:48 -0400

    final code

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+// Footer.js
+
+import React from 'react';
+import '../../styles//Footer.css'; 
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; {new Date().getFullYear()} ANJALI CHEPURI. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
